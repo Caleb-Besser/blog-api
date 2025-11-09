1a5c134a5bd843800728be1e6994d01b1f8d3a0d
Caleb-Besser <calebbesser.xk@gmail.com>
2025-11-08 16:49:29 -0800

    Added GET endpoints for all posts and posts by id

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ app.get("/test-db", async (req, res) => {
     }
 });
 
+app.get("/api/posts", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM posts");
+        res.json({
+            message: "Fetched all posts from database",
+            posts: result.rows,
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/api/posts/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query("SELECT * FROM posts WHERE id = $1", [
+            id,
+        ]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Post not found!" });
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 app.post("/api/posts", async (req, res) => {
     try {
         const { title, content } = req.body;
