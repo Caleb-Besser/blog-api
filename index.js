9bd8d438fd1552a885dd55a02220b34eb8514945
Caleb-Besser <calebbesser.xk@gmail.com>
2025-11-08 16:20:08 -0800

    Added PostgreSQL Database connection on db.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 
+const pool = require("./db");
+
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/test-db", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({ message: "Database connected!", time: result.rows[0] });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(port, () => {
