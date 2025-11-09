627b8152deaf7fe8a6b59608b1ccc96590939ee0
Caleb-Besser <calebbesser.xk@gmail.com>
2025-11-08 16:36:35 -0800

    Added POST endpoint for new blog posts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const pool = require("./db");
 
 const app = express();
 
+app.use(express.json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -14,6 +16,22 @@ app.get("/test-db", async (req, res) => {
     try {
         const result = await pool.query("SELECT NOW()");
         res.json({ message: "Database connected!", time: result.rows[0] });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post("/api/posts", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        if (!title || !content) {
+            res.status(500).json({ error: "wrong post body" });
+        }
+        const result = await pool.query(
+            "INSERT INTO posts (title, content) VALUES ($1, $2) RETURNING *",
+            [title, content]
+        );
+        res.json(result.rows[0]);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
